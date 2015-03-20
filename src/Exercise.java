f78dedf7ecabad0d709b6f4b72b295634bde3f25
cholpon <cholpon7@gmail.com>
2015-03-20 22:32:13 +0100

    added printStrings()

--- src/Exercise.java
+++ src/Exercise.java
@@ -1,29 +1,81 @@
-
 public class Exercise {
 
-	
-	public void printNumbers () {
-		
-		//iterate over numbers
-		for (int i = 1; i <=100; i++) {
-			
+	public void printNumbers() {
+
+		// iterate over numbers
+		for (int i = 1; i <= 100; i++) {
+
 			// both cases
 			if ((i % 3 == 0) && (i % 5 == 0)) {
-				System.out.println ("FizzBuzz");
+				System.out.println("FizzBuzz");
 			} else {
 				// case: multiple of 3
 				if (i % 3 == 0) {
-					System.out.println ("Fizz");
+					System.out.println("Fizz");
 				} else
-				// case: multipile of 5
+				// case: multiple of 5
 				if (i % 5 == 0) {
-					System.out.println ("Buzz");
+					System.out.println("Buzz");
 				} else {
-			    // default case: print number
-					System.out.println (i);
+					// default case: print number
+					System.out.println(i);
 				}
 			}
-			
-		}		
+		}
+	}
+
+	public String fizzBuzz(int i) {
+		// both cases
+		if ((i % 3 == 0) && (i % 5 == 0)) {
+			return ("FizzBuzz");
+		} else {
+			// case: multiple of 3
+			if (i % 3 == 0) {
+				return ("Fizz");
+			} else
+			// case: multiple of 5
+			if (i % 5 == 0) {
+				return ("Buzz");
+			} else {
+				// default case: print number
+				return (Integer.toString(i));
+			}
+		}
+	}
+
+	public void printNumbersRefactored() {
+
+		for (int i = 1; i <= 100; i++) {
+			System.out.println(fizzBuzz(i));
+		}
+	}
+
+	public String concatStrings(String a, String b) {
+
+		String res = "";
+		String smaller_str;
+		String bigger_str;
+
+		// determine which string is smaller
+		if (a.length() <= b.length()) {
+			smaller_str = a;
+			bigger_str = b;
+		} else {
+			smaller_str = b;
+			bigger_str = a;
+		}
+
+		// add characters from both strings, until smaller string is over
+		for (int i = 0; i < smaller_str.length(); i++) {
+			res = res + a.charAt(i) + b.charAt(i);
+		}
+
+		// add what is left of bigger string, if strings are not of the same
+		// length
+		if (smaller_str.length() != bigger_str.length()) {
+			res = res + bigger_str.substring(smaller_str.length());
+		}
+		//System.out.println(res);
+		return res;
 	}
 }
--- src/Test.java
+++ src/Test.java
@@ -6,46 +6,70 @@ import java.io.PrintStream;
 import org.junit.*;
 
 public class Test {
-	
-	//Stream to catch the System.output messages
+
+	// Stream to catch the System.output messages
 	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
 	PrintStream MyPrintStream = new PrintStream(outContent);
-    
-    //IMPORTANT: Save the old System.out!
-    PrintStream old = System.out;
 
-    // instantiate class object for testing
+	// IMPORTANT: Save the old System.out!
+	PrintStream old = System.out;
+
+	// instantiate class object for testing
 	Exercise ex_object = new Exercise();
-	
+
+	/*
 	@org.junit.Test
 	public void test() {
-	   // Tell Java to use your special stream
-	   System.setOut(MyPrintStream);
-	   
-	  // Print some output: goes to your special stream
-	   ex_object.printNumbers();
-	   
-	  // Put things back
-	  System.out.flush();
-	  System.setOut(old);
-	  
-	  // Show what happened
-	  //System.out.println("Here: " + outContent.toString());
-	  
-	  // Get result from stream in form of string
-	  String str_res= outContent.toString();
-	  
-	  // Split the string by line
-	  String [] res = str_res.split("\\r?\\n");
-	  
-	  // Assert Cases:
-	  assertEquals ("1", res[0]); // 1 is not multiple of 3 or 5
-	  assertEquals ("2", res[1]); // 2 is not multiple of 3 or 5
-	  assertEquals ("Fizz", res[2]); // 3 is multiple of 3
-	  assertEquals ("4", res[3]); // 4 is not multiple of 3 or 5
-	  assertEquals ("FizzBuzz", res[29]); // 30 is multiple of 3 and 5 -> FizzBuzz
-	  assertEquals ("Buzz", res[4]); // 5 is multiple of 5 -> Buzz
-	  
+		// Tell Java to use your special stream
+		System.setOut(MyPrintStream);
+
+		// Print some output: goes to your special stream
+		ex_object.printNumbers();
+
+		// Put things back
+		System.out.flush();
+		System.setOut(old);
+
+		// Show what happened
+		// System.out.println("Here: " + outContent.toString());
+
+		// Get result from stream in form of string
+		String str_res = outContent.toString();
+
+		// Split the string by line
+		String[] res = str_res.split("\\r?\\n");
+
+		// Assert Cases:
+		assertEquals("1", res[0]); // 1 is not multiple of 3 or 5
+		assertEquals("2", res[1]); // 2 is not multiple of 3 or 5
+		assertEquals("Fizz", res[2]); // 3 is multiple of 3
+		assertEquals("4", res[3]); // 4 is not multiple of 3 or 5
+		assertEquals("FizzBuzz", res[29]); // 30 is multiple of 3 and 5 ->
+											// FizzBuzz
+		assertEquals("Buzz", res[4]); // 5 is multiple of 5 -> Buzz
+
+	} */
+	
+	@org.junit.Test
+	public void testPrintNumbersRefactored() {
+		
+		assertEquals ("Fizz", ex_object.fizzBuzz(3));
+		assertEquals ("FizzBuzz", ex_object.fizzBuzz(30));
+		assertEquals ("Buzz", ex_object.fizzBuzz(5));
+		assertEquals ("1", ex_object.fizzBuzz(1));
 	}
 
+	@org.junit.Test
+	public void testStrings() {
+		String a = "abc";
+		String b = "xyz";	
+		String a2 = "Hi";
+		String b2 = "There";
+		String a3 = "xxxx";
+		
+		assertEquals ("axbycz", ex_object.concatStrings(a, b));
+		assertEquals ("HTihere", ex_object.concatStrings(a2,b2));
+		assertEquals ("xTxhxexre", ex_object.concatStrings(a3, b2));
+
+	}
 }
